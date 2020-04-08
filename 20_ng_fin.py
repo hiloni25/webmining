2e4255e8ed3ce7cb27b4c35c9941377857d8a553
hiloni25 <hilonimehta0205@gmail.com>
2020-04-07 20:42:57 -0400

    20_ng
    This is the code for 20_ng with accuracy of 95.6%

--- 20_ng_fin.py
+++ 20_ng_fin.py
@@ -22,13 +22,14 @@ import nltk,re
 from nltk.tokenize import sent_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neural_network import MLPClassifier
+from sklearn.linear_model import LogisticRegression
 
 
 #read the reviews and their polarities from a given file
 def getlabel(x):
-    if x=='comp.windows.x' or x=='comp_os.ms-windows.misc' or x=='comp.sys.ibm.pc.hardware' or x=='comp.sys.mac.hardware' or x=='comp.graphics':
+    if x=='comp.windows.x' or x=='comp_os.ms-windows.misc' or x=='comp.sys.ibm.pc.hardware'  or x=='comp.graphics': #or x=='comp.sys.mac.hardware'
         return 'comp'
-    elif x=='rec.sport.baseball' or x=='rec.sports.hockey':
+    elif x=='rec.sport.baseball' or x=='rec.sport.hockey':
         return 'sport'
     elif x=='talk.politics.misc' or x=='talk.politics.mideast' or x=='talk.politics.guns' :
         return 'politics'
@@ -49,45 +50,23 @@ def loadData(parent_dir):
 
         
         for file in kid_dirs:
-            print(path,file)
             files=os.path.join(path,file)
 
             news=[]
             first_emptyLine=False
             f=open(files,encoding="ISO-8859-1")
+            read = f.read().strip()
+            #print(read)
             
-            for line in f:
-                if not first_emptyLine:
-                    if line=='\n':
-                        first_emptyLine = True
-                        continue
-                    else:
-                        continue
-                else:
-                    r = line.strip().lower()
-                    news.append(re.sub('[^a-z]',' ',r))
-            
-            #print(news)
-            '''
-            sentences = ""
-            if(len(news) != 0):
-                sentences=sent_tokenize(news)   
-                #print(sentences)
-                
-            for sentence in sentences:
-        
-                terms = nltk.word_tokenize(sentence) 
-             
-            print(terms)
-            #print(len(terms),len(terms[0]))
-            termL = list(chain.from_iterable(terms))'''
+            read.split('/n/n')
+            #news.append()
+           
             if kid_dir== 'comp.windows.x' or kid_dir== 'rec.sport.baseball' or kid_dir== 'talk.politics.misc' or kid_dir=='rec.autos': 
-                text_test.append(''.join(news))
+                text_test.append(read[1:])
                 #text_test.append(termL)
                 label_test.append(getlabel(kid_dir))
-            elif kid_dir=='comp.sys.ibm.pc.hardware' or kid_dir=='comp.sys.mac.hardware' or kid_dir=='comp.graphics' or kid_dir=='rec.sports.hockey' or kid_dir=='talk.politics.mideast' or kid_dir=='talk.politics.guns'  or kid_dir=='rec.autos' or kid_dir=='rec.motorcycles':
-                text_train.append(''.join(news))
-                #text_train.append(termL)
+            elif kid_dir=='comp.sys.ibm.pc.hardware'  or kid_dir=='comp.graphics' or kid_dir=='rec.sport.hockey' or kid_dir=='talk.politics.mideast' or kid_dir=='talk.politics.guns'  or kid_dir=='rec.autos' or kid_dir=='rec.motorcycles':
+                text_train.append(read[1:])
                 label_train.append(getlabel(kid_dir))
             else:
                 continue
@@ -98,11 +77,11 @@ def loadData(parent_dir):
 
 text_tests, label_tests, text_trains, label_trains=loadData('/Users/Hilony/Downloads/20_newsgroups')
 
-#counter = CountVectorizer()
-#counter.fit(text_trains)
-
-counter = TfidfVectorizer()
+counter = CountVectorizer()
 counter.fit(text_trains)
+
+#counter = TfidfVectorizer()
+#counter.fit(text_trains) 
 
 
 #count the number of times each term appears in a document and transform each doc into a count vector
@@ -110,7 +89,8 @@ counts_train = counter.transform(text_trains)#transform the training data
 counts_test = counter.transform(text_tests)#transform the testing data
 
 #train classifier
-clf = MLPClassifier() #62
+#clf = MLPClassifier() #62
+clf = LogisticRegression(solver='liblinear')
 
 #clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=5) #60%
 #clf = clf = MultinomialNB()
